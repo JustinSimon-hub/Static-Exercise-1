85746dea96a874ee40312d9fe14f5f1eef05437c
Justin Simon <justinsimon@Justins-MBP.lan>
2023-04-23 21:44:57 -0700

    Finished second runthrough of assignment, works better than first

--- Program.cs
+++ Program.cs
@@ -4,20 +4,34 @@
     {
         static void Main(string[] args)
         {
-           var farenheit = TempConverter.CelsiusToFahrenheit = 15.7;
-           var celsius = TempConverter.FahrenheitToCelsius = 90.5;
-          Console.WriteLine("Before conversion");
-           Console.WriteLine(farenheit);
-           Console.WriteLine("***********");
-           Console.WriteLine("After conversion");
-           Console.WriteLine(celsius);
-       
+            
+            
+//Uses the static members of the TempConverter class, meaning that no instantiation is neccessary to use the members
+//Asks the user for the tempature in the format they would like converted 
+//The users input is stored as a varible for the static members: farenheight and celsius 
+//Those values are then plugged into the conversion methods and printed to the screen
+
+
+
+     Console.WriteLine("What tempature in celsius would you like converted to farenhight?");
+            TempConverter.celsius = double.Parse(Console.ReadLine());
+           Console.WriteLine($" The tempature for celsius converted to farenheight is: {TempConverter.CelsiusToFarenheit(TempConverter.celsius)}");
+           Console.WriteLine("----------------------------------");
+           Console.WriteLine("What tempature in farenheight would you like converted to celsius?");
+           TempConverter.farenheight = double.Parse(Console.ReadLine());
+           Console.WriteLine($"The tempature for farenheight converted to celsius is: {TempConverter.FahrenheitToCelsius(TempConverter.farenheight)}");
         }
     }
 }
-/*Create a static class called TempConverter. 
+/*
+The class should have at least 2 methods, one called FahrenheitToCelsius 
+that will require a double as a parameter and return a double, 
+the other CelsiusToFahrenheit which will also require 
+a double as a parameter and return a double.
 
-The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter and return a double, the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+Fill out these methods and call them in your Program.cs file. 
+Make sure they’re accurate!
 
-Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!
+For reference:
+https://github.com/mvdoyle/StaticExercise
 */
--- TempConverter.cs
+++ TempConverter.cs
@@ -5,24 +5,35 @@ namespace StaticExercise
 	{
 		 static TempConverter()
 		{
-			
 		}
-		private static double far2cel;
-		public static double FahrenheitToCelsius 
+		//This assignment uses static access meaning that there is no requirment for a instantiation to access these members 
+		//and the members actually belong to the class and not the object of the class
+		//In this problem the static props. farenheight and celsius are going to be initialized via the Console.ReadLine method and then plugged into the two methods below to convert them
+		public  static double farenheight {get; set;}
+		public static double celsius {get; set;}
+		public static double FahrenheitToCelsius(double farenheight)
 		{
-				get{return far2cel;}
-				set{far2cel = value;}
+				  double b = (farenheight - 32) * 5 / 9;
+				 return b;
 		}
-		private static double cel2far;
-		public static double CelsiusToFahrenheit 
+		public static double CelsiusToFarenheit(double celsius)
 		{
-			get{return cel2far;}
-			set{cel2far = value;}
+				double d = (celsius * 9 / 5) + 32;
+    			return d;
 		}
-		
-		
 	}
 }
 
 
-//The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter and return a double, the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+/*
+The class should have at least 2 methods, one called FahrenheitToCelsius 
+that will require a double as a parameter and return a double, 
+the other CelsiusToFahrenheit which will also require 
+a double as a parameter and return a double.
+
+Fill out these methods and call them in your Program.cs file. 
+Make sure they’re accurate!
+
+For reference:
+https://github.com/mvdoyle/StaticExercise
+*/
